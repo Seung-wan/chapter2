25fefa344d7d5ec0337bb35c97eea2dc5e6f2edc
Seung-wan <tmddhks0104@gmail.com>
2024-03-21 00:28:29 +0900

    미션 C 진행

--- packages/chapter2/src/MyReact.js
+++ packages/chapter2/src/MyReact.js
@@ -1,11 +1,21 @@
-import { createHooks } from "./hooks";
-import { render as updateElement } from "./render";
+import { createHooks } from './hooks';
+import { render as updateElement, createElement } from './render';
 
 function MyReact() {
   const _render = () => {};
-  function render($root, rootComponent) {}
 
-  const { useState, useMemo, resetContext: resetHookContext } = createHooks(_render);
+  function render($root, rootComponent) {
+    const jsx = rootComponent();
+    const element = createElement(jsx);
+
+    $root.appendChild(element);
+  }
+
+  const {
+    useState,
+    useMemo,
+    resetContext: resetHookContext,
+  } = createHooks(_render);
 
   return { render, useState, useMemo };
 }
