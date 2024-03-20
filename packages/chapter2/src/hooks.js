9af34bea5c7054b87e61f01666d0bb4fa477a4ba
Seung-wan <tmddhks0104@gmail.com>
2024-03-21 00:28:21 +0900

    미션 B 구현

--- packages/chapter2/src/hooks.js
+++ packages/chapter2/src/hooks.js
@@ -1,15 +1,57 @@
 export function createHooks(callback) {
+  let options = {
+    states: [],
+    currentStateIndex: 0,
+    memos: [],
+    currentMemoIndex: 0,
+  };
+
   const useState = (initState) => {
-    return [];
+    const { currentStateIndex, states } = options;
+    if (states.length === currentStateIndex) {
+      states.push(initState);
+    }
+
+    const state = states[currentStateIndex];
+    const setter = (newState) => {
+      states[currentStateIndex] = newState;
+    };
+
+    options.currentStateIndex += 1;
+
+    return [state, setter];
   };
 
   const useMemo = (fn, refs) => {
-    return fn();
+    let { memos, memoDeps, currentMemoIndex } = options;
+
+    if (memos[currentMemoIndex] === undefined) {
+      const nextValue = fn();
+
+      memos[currentMemoIndex] = [nextValue, refs];
+      currentMemoIndex += 1;
+
+      return nextValue;
+    }
+
+    const nextRefs = refs;
+    const [prevValue, prevRefs] = memos[currentMemoIndex];
+    if (prevRefs.every((prev, idx) => prev === nextRefs[idx])) {
+      currentMemoIndex += 1;
+      return prevValue;
+    }
+
+    const nextValue = fn();
+
+    memos[currentMemoIndex] = [nextValue, nextRefs];
+    currentMemoIndex += 1;
+    return nextValue;
   };
 
   const resetContext = () => {
-
-  }
+    options.currentMemoIndex = 0;
+    options.currentStateIndex = 0;
+  };
 
   return { useState, useMemo, resetContext };
 }
